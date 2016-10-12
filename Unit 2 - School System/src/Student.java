92431f3137f9e23f95e22b605cc59c7c40e62dbb
323016139 <323016139@C-9S608J1.tdsb.on.ca>
2016-10-12 15:05:50 -0400

    School System, and Student Classes

---
+++ Unit 2 - School System/src/Student.java
@@ -0,0 +1,97 @@
+public class Student implements Comparable {
+	
+	private String firstName, lastName, streetName, city, province, postalCode, phoneNumber, birthdate;
+	private long studentNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getPostalCode() throws InvalidInputException {
+	int postalCodeLength = postalCode.length();
+	if (postalCodeLength == 7){
+		return postalCode;
+	}
+	else;
+		throw new InvalidInputException("Wrong format!");
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhoneNumber() throws InvalidInputException {
+	int phoneNumberLenght = phoneNumber.length();
+	if (phoneNumberLenght == 12){
+		return phoneNumber;
+	}
+	else;
+		throw new InvalidInputException("Wrong format!");
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getBirthdate() throws InvalidInputException {
+	int birthdayLenght = birthdate.length();
+	if (birthdayLenght == 10){
+		return birthdate;
+	}
+		else;
+			throw new InvalidInputException("Wrong format!");
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public long getStudentNumber() {
+		return studentNumber;
+	}
+
+	public void setStudentNumber(long studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if(this.getLastName())
+		return 0;
+	}
+}
---
+++ Unit 2 - School System/src/StudentInfo.java
@@ -0,0 +1,149 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentInfo {
+	public static String scan;
+	public static int i = 0;
+	public static ArrayList<Student> studRec = new ArrayList<Student>();
+	private static long idGenerator = 300000000;
+
+	public static void main(String[] args) throws InvalidInputException {
+	String run = ("true");
+	do{
+		System.out.println("Choose an option");
+		System.out.println("");
+		System.out.println("1.  Enter a new student");
+		System.out.println("2.  Print a student's info (Only use if a sutudent is in the system)");
+		System.out.println("3.  Print all student's info");
+		System.out.println("4.  Option 4");
+		System.out.println("5.  Option 5");
+		System.out.println("6.  Option 6");
+		System.out.println("7.  Option 7");
+		System.out.println("8.  Option 8");
+		System.out.println("9.  Option 9");
+		System.out.println("10. Quit");
+		Scanner option = new Scanner(System.in);
+		int optionInput = option.nextInt();
+			
+		switch (optionInput){
+		case 1:
+			addStudent();
+			break;
+		case 2:
+			printAStudent();
+			break;
+		case 3:
+			printAllStudents();
+			break;
+		case 4:
+			System.out.println("Not available yet");
+			System.out.println("");
+			break;
+		case 5:
+			System.out.println("Not available yet");
+			System.out.println("");
+			break;
+		case 6:
+			System.out.println("Not available yet");
+			System.out.println("");
+			break;
+		case 7:
+			System.out.println("Not available yet");
+			System.out.println("");
+			break;
+		case 8:
+			System.out.println("Not available yet");
+			System.out.println("");
+			break;
+		case 9:
+			System.out.println("Not available yet");
+			System.out.println("");
+			break;
+		case 10:
+			run = "false";
+			System.out.println("Thank you for using my program!");
+			return;
+		}
+	}while (run == "true");
+}
+	
+	public static void addStudent() throws InvalidInputException{
+	studRec.add(new Student());
+	Scanner scan = new Scanner(System.in);
+
+	idGenerator++;
+	studRec.get(studRec.size()-1).setStudentNumber(idGenerator);
+	
+	System.out.print("Enter the student's first name: ");
+	studRec.get(studRec.size()-1).setFirstName(scan.nextLine());
+	
+	System.out.print("Enter the student's last name: ");
+	studRec.get(studRec.size()-1).setLastName(scan.nextLine());
+	
+	System.out.print("Enter the student's street name: ");
+	studRec.get(studRec.size()-1).setStreetName(scan.nextLine());
+	
+	System.out.print("Enterthe student's city: ");
+	studRec.get(studRec.size()-1).setCity(scan.nextLine());
+	
+	System.out.print("Enter the student's province: ");
+	studRec.get(studRec.size()-1).setProvince(scan.nextLine());
+	
+	System.out.print("Enter the student's postal code: (EX: x#x x#x)");
+	studRec.get(studRec.size()-1).setPostalCode(scan.nextLine());
+
+	System.out.print("Enter the student's phone number: (EX: ### ### ####)");
+	studRec.get(studRec.size()-1).setPhoneNumber(scan.nextLine());
+	
+	System.out.print("Enter the student's birthdate: (EX: dd/mm/yyyy)");
+	studRec.get(studRec.size()-1).setBirthdate(scan.nextLine());
+	
+	System.out.println("What you have entered:");
+	System.out.println(studRec.get(studRec.size()-1).getFirstName());
+	System.out.println(studRec.get(studRec.size()-1).getLastName());
+	System.out.println(studRec.get(studRec.size()-1).getStreetName());
+	System.out.println(studRec.get(studRec.size()-1).getCity());
+	System.out.println(studRec.get(studRec.size()-1).getProvince());
+	System.out.println(studRec.get(studRec.size()-1).getPostalCode());
+	System.out.println(studRec.get(studRec.size()-1).getPhoneNumber());
+	System.out.println(studRec.get(studRec.size()-1).getBirthdate());
+	System.out.println("");
+	
+	}
+	
+	public static void printAStudent() throws InvalidInputException{
+		
+		System.out.print("Which student whould you like to print? (Student Number)");
+		Scanner scan = new Scanner(System.in);
+		int lookingStudNum = scan.nextInt();
+		
+		System.out.println(studRec.get(lookingStudNum - 1).getFirstName());
+		System.out.println(studRec.get(lookingStudNum - 1).getLastName());
+		System.out.println(studRec.get(lookingStudNum - 1).getStreetName());
+		System.out.println(studRec.get(lookingStudNum - 1).getCity());
+		System.out.println(studRec.get(lookingStudNum - 1).getProvince());
+		System.out.println(studRec.get(lookingStudNum - 1).getPostalCode());
+		System.out.println(studRec.get(lookingStudNum - 1).getPhoneNumber());
+		System.out.println(studRec.get(lookingStudNum - 1).getBirthdate());
+		System.out.println(studRec.get(lookingStudNum - 1).getStudentNumber());
+		System.out.println("");
+	}
+	
+	public static void printAllStudents() throws InvalidInputException{
+		for(i = 0; i < studRec.size(); i++) // NO ; AFTER A FOR LOOP!!!
+		{
+			System.out.print(i + 1);
+			System.out.println(". ");
+			System.out.println("     " + studRec.get(i).getFirstName());
+			System.out.println("     " + studRec.get(i).getLastName());
+			System.out.println("     " + studRec.get(i).getStreetName());
+			System.out.println("     " + studRec.get(i).getCity());
+			System.out.println("     " + studRec.get(i).getProvince());
+			System.out.println("     " + studRec.get(i).getPostalCode());
+			System.out.println("     " + studRec.get(i).getPhoneNumber());
+			System.out.println("     " + studRec.get(i).getBirthdate());
+			System.out.println("     " + studRec.get(i).getStudentNumber());
+			System.out.println("");
+		}
+	}
+}
