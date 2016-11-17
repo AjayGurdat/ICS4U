197cddd8e4713779d763bcc0e26fe814988faef5
323016139 <323016139@C-9S608J1.tdsb.on.ca>
2016-11-17 14:55:36 -0500

    Today's class slight updates

--- Unit 3 - Sorting and Searching/src/Searches.java
+++ Unit 3 - Sorting and Searching/src/Searches.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * @author 323016139
  */
 public class Searches {
-	
+
 	private static Scanner scan;
 
 	/**
@@ -19,6 +19,10 @@ public class Searches {
 	public static void main(String[] args) {
 		int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 		double[] numbersDouble = {0.24, 2.314, 4.52, 5.4, 3.5, 3.2, 4.7, 2.6, 2.4, 13.86, 2.4, 3.4};
+		
+		int startIndex = 0;
+		int endIndex = numbers.length;
+		int midIndex = (startIndex + endIndex)/2;
 
 		scan = new Scanner(System.in);
 		System.out.println("Which array would you like to use?");
@@ -26,137 +30,113 @@ public class Searches {
 		System.out.println("2. Double");
 		System.out.println("3. String");
 		int prefSearch = scan.nextInt();
-		
-		int startIndex = 0;
-		int endIndex = numbers.length - 1;
-		int midIndex = (startIndex + endIndex)/2;
-		int endIndexDouble = numbersDouble.length - 1;
-		int midIndexDouble = (startIndex + endIndexDouble)/2;
+
+		switch (prefSearch){
+		case 1: searchInt(numbers, startIndex, endIndex, midIndex);
+		break;
+		case 2: searchDouble();
+		break;
+		case 3: searchString ();
+		break;
+		}
+
 		scan = new Scanner(System.in);
 		System.out.println("What is your target number?");
-		double inputTarget = scan.nextDouble();
+		double target = scan.nextDouble();
 
 		System.out.println("How would you like to find your target?");
 		System.out.println("1. Linear Search");
 		System.out.println("2. Binary Search");
 		scan = new Scanner(System.in);
 		int prefMode = scan.nextInt();
-		
-		int foundIndex = 0;
-		if (prefMode == 1 && prefSearch == 1){
-			foundIndex = linearSearch(numbers, inputTarget);
-		}else if (prefMode == 1 && prefSearch == 2){
-			foundIndex = binarySearch(startIndex, endIndex, midIndex, inputTarget);
-		}else if (prefMode == 2 && prefSearch == 1){
-			foundIndex = linearSearchDouble(numbersDouble, inputTarget);
-		}else if (prefMode == 2 && prefSearch == 2){
-			foundIndex = binarySearchDouble(startIndex, endIndexDouble, midIndexDouble, inputTarget);
-		}else{
-			System.out.println("You didn't select a valid answer.");
-		}
-
-		if (foundIndex >= 0){ 
-			System.out.print("Your target was found at spot " + foundIndex + ".");
-		}else{
-			System.out.print("Your target was not in the array.");
-		}
-	}
-	/**
-	 * 
-	 * @param startIndex
-	 * @param endIndexDouble
-	 * @param midIndexDouble
-	 * @param inputTarget
-	 * @return
-	 * This method does binary searching in the double array
-	 */
-	private static int binarySearchDouble(int startIndex, int endIndexDouble, int midIndexDouble, double inputTarget) {
-
-		
-		if (startIndex > endIndexDouble){
-			return -1; //Returns -1 is target is not found
-		}
-		
-		if (midIndexDouble == inputTarget){
-			return midIndexDouble; // Returns when target index is found
-
-		}else if (midIndexDouble > inputTarget){
-			return binarySearchDouble(startIndex, midIndexDouble - 1, (startIndex + (midIndexDouble -1))/2, inputTarget); //Calls back method with new variables
-
-		}else if (midIndexDouble < inputTarget){
-			return binarySearchDouble(midIndexDouble + 1, endIndexDouble, ((midIndexDouble +1) + endIndexDouble)/2, inputTarget); //Calls back method with new variables
-		}
-		return -1; //Returns -1 is target is not found
-
 	}
 
-	/**
-	 * 
-	 * @param numbersDouble
-	 * @param inputTarget
-	 * @return
-	 */
-	private static int linearSearchDouble(double[] numbersDouble, double inputTarget) {
-
-		for(int i = 0; i < numbersDouble.length; i++){
-			if (numbersDouble[i] == (inputTarget)){
-				int foundIndex = i;
-				return foundIndex; //Returns index where target is found
-			}else if (i > numbersDouble.length){ // Returns -1 if target is not found
-				int foundIndex = -1;
-				return foundIndex;
-			}
-		}
-		return -1; //Returns -1 is target is not found
-	
+	private static void searchString() {
+		System.out.println("How would you like to find your target?");
+		System.out.println("1. Linear Search");
+		System.out.println("2. Binary Search");
+		scan = new Scanner(System.in);
+		int prefMode = scan.nextInt();
 	}
-	/**
-	 * 
-	 * @param startIndex
-	 * @param endIndex
-	 * @param midIndex
-	 * @param inputTarget
-	 * @return
-	 * This method does binary searching to find the target number in the array
-	 */
-	private static int binarySearch(int startIndex, int endIndex, int midIndex, double inputTarget) {
-		
-		if (startIndex > endIndex){
-			return -1; //Returns -1 is target is not found
-		}
-		
-		if (midIndex == inputTarget){
-			return midIndex; // Returns when target index is found
 
-		}else if (midIndex > inputTarget){
-			return binarySearch(startIndex, midIndex - 1, (startIndex + (midIndex -1))/2, inputTarget); //Calls back method with new variables
+private static void searchDouble() {
+	System.out.println("How would you like to find your target?");
+	System.out.println("1. Linear Search");
+	System.out.println("2. Binary Search");
+	scan = new Scanner(System.in);
+	int prefMode = scan.nextInt();
 
-		}else if (midIndex < inputTarget){
-			return binarySearch(midIndex + 1, endIndex, ((midIndex +1) + endIndex)/2, inputTarget); //Calls back method with new variables
-		}
-		return -1; //Returns -1 is target is not found
-
+}
+/**
+ * 
+ * @param numbers
+ * @param startIndex
+ * @param endIndex
+ * @param midIndex
+ */
+private static void searchInt(int[] numbers, int startIndex, int endIndex, int midIndex) {
+	System.out.println("How would you like to find your target?");
+	System.out.println("1. Linear Search");
+	System.out.println("2. Binary Search");
+	scan = new Scanner(System.in);
+	int prefMode = scan.nextInt();
+	System.out.println("What is your target number?");
+	scan = new Scanner(System.in);
+	double target = scan.nextDouble();
+	switch (prefMode){
+	case 1: linearSearch(numbers, target);
+	break;
+	case 2: binarySearch(startIndex, endIndex, midIndex, target);
 	}
-	/**
-	 * 
-	 * @param numbers
-	 * @param inputTarget
-	 * @return
-	 * This method does linear searching to find the target number in the array
-	 */
-	public static int linearSearch(int[] numbers, double inputTarget){
-		for(int i = 0; i < numbers.length; i++){
-			if (numbers[i] == (inputTarget)){
-				int foundIndex = i;
-				return foundIndex; //Returns index where target is found
-			}else if (i > numbers.length){ // Returns -1 if target is not found
-				int foundIndex = -1;
-				return foundIndex;
 
-			}
+}
+/**
+ * 
+ * @param startIndex
+ * @param endIndex
+ * @param midIndex
+ * @param inputTarget
+ * @return
+ * This method does binary searching to find the target number in the array
+ */
+private static int binarySearch(int startIndex, int endIndex, int midIndex, double inputTarget) {
 
-		}
+	if (startIndex > endIndex){
 		return -1; //Returns -1 is target is not found
 	}
+
+	if (midIndex == inputTarget){
+		return midIndex; // Returns when target index is found
+
+	}else if (midIndex > inputTarget){
+		return binarySearch(startIndex, midIndex - 1, (startIndex + (midIndex -1))/2, inputTarget); //Calls back method with new variables
+
+	}else if (midIndex < inputTarget){
+		return binarySearch(midIndex + 1, endIndex, ((midIndex +1) + endIndex)/2, inputTarget); //Calls back method with new variables
+	}
+	return -1; //Returns -1 is target is not found
+
+}
+/**
+ * 
+ * @param numbers
+ * @param inputTarget
+ * @return
+ * This method does linear searching to find the target number in the array
+ */
+public static int linearSearch(int[] numbers, double target){
+	for(int i = 0; i < numbers.length; i++){
+		if (numbers[i] == (target)){
+			int foundIndex = i;
+			return foundIndex; //Returns index where target is found
+		}else if (i > numbers.length){ // Returns -1 if target is not found
+			int foundIndex = -1;
+			return foundIndex;
+
+		}
+
+	}
+	return -1; //Returns -1 is target is not found
+}
 
 }
