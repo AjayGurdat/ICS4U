aebc154f688bad2e54e8d7511ea01d8cbbaebe78
AjayGurdat <ajaygurdat@hotmail.com>
2016-12-20 21:55:27 -0500

    Update Dodgeball.java

--- Hockey/src/Dodgeball.java
+++ Hockey/src/Dodgeball.java
@@ -1,41 +1,131 @@
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Dodgeball extends GamePanel implements MouseMotionListener {
-	private static final long serialVersionUID = 1L;
-	
-	final int pauseDuration = 50;
-	static int width = 500;
-	static int height = 300;
-	final int numBalls = 5;
+/**
+ * This is the beginning of a simple game. You should expand it into a dodgeball
+ * game, where the user controls an object with the mouse or keyboard, and tries
+ * to avoid the balls flying around the screen. If they get hit, it's game over.
+ * If they survive for 20 seconds (or some other fixed time), they go on to the
+ * next level. <br>
+ * <br>
+ * Should be run at around 500x300 pixels.<br>
+ * <br>
+ * 
+ * @version Nov. 2015
+ * 
+ * @author Christina Kemp adapted from Sam Scott
+ */
+@SuppressWarnings("serial")
+public class GamePanel extends JPanel implements Runnable, MouseMotionListener {
+
 	int x = -1, y = -1;
+	int width = 500;
+	int height = 300;
+
+	/**
+	 * The number of balls on the screen.
+	 */
+	final int numBalls = 1;
+	/**
+	 * The pause between repainting (should be set for about 30 frames per
+	 * second).
+	 */
+	final int pauseDuration = 50;
+	/**
+	 * An array of balls.
+	 */
 	FlashingBall[] ball = new FlashingBall[numBalls];
 
+	/** main program (entry point) */
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Dodgeball new");
+
+		// Set up main window (using Swing's Jframe)
+		JFrame frame = new JFrame("Dodgeball");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(new Dimension(width, height));
+		frame.setSize(new Dimension(500, 300));
 		frame.setAutoRequestFocus(false);
 		frame.setVisible(true);
 		Container c = frame.getContentPane();
 		c.add(new GamePanel());
 		frame.pack();
+
+	}
+
+	public GamePanel() {
+		// Start the ball bouncing (in its own thread)
+		this.setPreferredSize(new Dimension(width, height));
+		this.setBackground(Color.WHITE);
+
+		for (int i = 0; i < numBalls; i++) {
+			ball[i] = new FlashingBall(50, 50, 0, width, 0, height);
+			ball[i].setXSpeed(Math.random() * 16 - 8);
+			ball[i].setYSpeed(Math.random() * 16 - 8);
+			ball[i].setColor(
+					new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
+		}
+
+		Thread gameThread = new Thread(this);
+		gameThread.start();
+
+	}
+
+	/**
+	 * Repaints the frame periodically.
+	 */
+	public void run() {
+		while (true) {
+			repaint();
+			try {
+				Thread.sleep(pauseDuration);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	/**
+	 * Clears the screen and paints the balls.
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		for (int i = 0; i < numBalls; i++) {
+			ball[i].draw(g);
+		}
+
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		x = super.getX();
-		y = super.getY();
+		x = e.getX();
+		y = e.getY();
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		x = super.getX();
-		y = super.getY();
+		x = e.getX();
+		y = e.getY();
 	}
+
+	public boolean hitBoxCheck(MouseEvent e){
+		for(int i = 0; i < numBalls; i++) {
+			double ballPositionX = ball[i].getX();
+			double mousePositionX = e.getX();
+			double ballPositionY = ball[i].getY();
+			double mousePositionY = e.getY();
+			double xLine = ballPositionX - mousePositionX;
+			double yLine = ballPositionY - mousePositionY;
+			double hypotenuse = Math.sqrt(Math.pow(xLine, 2) + Math.pow(yLine, 2));
+			if (hypotenuse >= 10){
+				return true;
+			}
+		}
+	}
+
 }
