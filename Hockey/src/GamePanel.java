f0fb3c9afe5775859e365d0a91446492328c02e8
AjayGurdat <ajaygurdat@hotmail.com>
2016-12-24 18:43:41 -0500

    Comments added, final copy, unless I find another bug
    The mouse must be moving always for the game to run smoothly

--- Hockey/src/GamePanel.java
+++ Hockey/src/GamePanel.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
@@ -11,35 +12,60 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
- * This is the beginning of a simple game. You should expand it into a dodgeball
- * game, where the user controls an object with the mouse or keyboard, and tries
- * to avoid the balls flying around the screen. If they get hit, it's game over.
- * If they survive for 20 seconds (or some other fixed time), they go on to the
- * next level. <br>
+ * A simple game where the objective is the survive for as long as possible my
+ * moving the mouse to not hit any of the balls on the screen. <br>
  * <br>
- * Should be run at around 500x300 pixels.<br>
- * <br>
+ * The mouse MUST BE CONSTANTLY MOVED for the program to continue to update what
+ * is on the screen. <br>
  * 
- * @version Nov. 2015
+ * @version Dec. 2019
  * 
- * @author Christina Kemp adapted from Sam Scott
+ * @author Ajay Gurdat adapted from Christina Kemp adapted from Sam Scott
  */
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel implements Runnable, MouseMotionListener, MouseListener {
 
+	/**
+	 * The current radius of the ball for a level.
+	 */
 	int levelRadius = 10;
+	/**
+	 * The current level.
+	 */
 	int level = 0;
+	/**
+	 * The time it takes to move onto the next level.
+	 */
 	int distanceBetweenLevels = 20;
+	/**
+	 * Time remaining until the next level.
+	 */
 	int nextLevelTime = 1;
+	/**
+	 * Current time alive.
+	 */
 	int time = 0;
-
+	/**
+	 * If the player is alive.
+	 */
 	boolean alive;
+	/**
+	 * If they player died form hitting a ball.
+	 */
 	boolean dieByBall = false;
+	/**
+	 * Initial starting positions for the mouse.
+	 */
 	int x = -1, y = -1;
+	/**
+	 * Width of the JPanel
+	 */
 	static int width = 1280;
-	static int height = 700;
-
+	/**
+	 * Height of the JPanel.
+	 */
+	static int height = 900;
 	/**
 	 * The number of balls on the screen.
 	 */
@@ -52,9 +78,6 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 	/**
 	 * An array of balls.
 	 */
-
-	boolean filled = false;
-
 	FlashingBall[] ball = new FlashingBall[numBalls];
 
 	/** main program (entry point) */
@@ -71,6 +94,9 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 
 	}
 
+	/**
+	 * Sets up the JPanel for the game to run in
+	 */
 	public GamePanel() {
 		// Start the ball bouncing (in its own thread)
 		this.setPreferredSize(new Dimension(width, height));
@@ -83,9 +109,10 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 			ball[i].setColor(
 					new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
 		}
-
+		// Creates the thread that the balls will run in
 		Thread gameThread = new Thread(this);
 		gameThread.start();
+		// Starts the MouseMotionListener and MouseListener
 		addMouseMotionListener(this);
 		addMouseListener(this);
 	}
@@ -113,12 +140,25 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 			g.setColor(new Color(0, 0, 0));
 		} else {
 			g.setColor(new Color(255, 255, 255));
+			g.setFont(new Font("TimesRoman", Font.ITALIC, 60));
+			g.drawString("YOU ARE DEAD!!!", 410, 450);
+			g.drawString("Level: " + level, 10, 50);
+			g.drawString("Points/Time: " + time, 10, 150);
+			return;
 		}
 		g.drawString("Level: " + level, 10, 20);
 		g.drawString("Points/Time: " + time, 10, 40);
 
 	}
 
+	/**
+	 * Checks if the mouse in within the radius of all of the balls, will return
+	 * false if the mouse had been hit, otherwise returns true
+	 * 
+	 * @param e
+	 *            The mouse event
+	 * @return
+	 */
 	public boolean hitBoxCheck(MouseEvent e) {
 		for (int i = 0; i < numBalls; i++) {
 			double ballPositionX = ball[i].getX();
@@ -135,6 +175,11 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 		return true;
 	}
 
+	/**
+	 * Runs if the mouse had been dragged.<br>
+	 * Tests if the mouse had been hit, if the mouse was hit, changed the
+	 * background and all of the balls color
+	 */
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		boolean space = hitBoxCheck(e);
@@ -144,13 +189,20 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 			alive = false;
 			dieByBall = true;
 			System.out.println("You got hit!");
+			// Sets the background to black
 			this.setBackground(Color.RED);
+			// Sets all of the balls color to black
 			for (int i = 0; i < numBalls; i++) {
 				ball[i].setColor(new Color(0, 0, 0));
 			}
 		}
 	}
 
+	/**
+	 * Runs if the mouse had been dragged.<br>
+	 * Tests if the mouse had been hit, if the mouse was hit, changed the
+	 * background and all of the balls color
+	 */
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		boolean space = hitBoxCheck(e);
@@ -160,13 +212,19 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 			alive = false;
 			dieByBall = true;
 			System.out.println("You got hit!");
+			// Sets the background to black
 			this.setBackground(Color.RED);
+			// Sets all of the balls color to black
 			for (int i = 0; i < numBalls; i++) {
 				ball[i].setColor(new Color(0, 0, 0));
 			}
 		}
 	}
 
+	/**
+	 * Tests to see if the player qualifies to move onto the next level, if yes,
+	 * increases all of the balls radius and sets a new goal for the next level
+	 */
 	public void doRun() {
 		if (alive == true) {
 			run();
@@ -175,6 +233,7 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 				level++;
 				nextLevelTime = nextLevelTime + distanceBetweenLevels;
 				levelRadius++;
+				// Increases the radius of all od the balls
 				for (int i = 0; i < numBalls; i++) {
 					ball[i].setRadius(levelRadius);
 					ball[i].setRadius(levelRadius);
@@ -196,6 +255,10 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 
 	}
 
+	/**
+	 * If the player was killed by the ball, the player will stay dead,
+	 * otherwise, revives the player
+	 */
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		if (dieByBall == false) {
@@ -204,6 +267,9 @@ public class GamePanel extends JPanel implements Runnable, MouseMotionListener, 
 		System.out.println("Entered");
 	}
 
+	/**
+	 * Kills the player when the player exits the JPanel
+	 */
 	@Override
 	public void mouseExited(MouseEvent e) {
 		alive = false;
